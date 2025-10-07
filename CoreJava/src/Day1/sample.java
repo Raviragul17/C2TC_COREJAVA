1faa9aa98907ce80bd90bc9123bf481af95c7304
Raviragul17 <your_github_email@example.com>
2025-10-07 14:42:08 +0530

    DAY 2 AND DAY 3 CODE

---
+++ CoreJava/src/Day1/sample.java
@@ -0,0 +1,9 @@
+package Day1;
+
+public class sample {
+public static void main(String[] args) {
+		
+		System.out.println("This is my First Code");
+
+}
+}
---
+++ CoreJava/src/Daytwo/IfDemo.java
@@ -0,0 +1,15 @@
+package Daytwo;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		
+		int number = 10;
+		
+		if (number > 0) {
+            System.out.println(number + " is a positive number.");
+        }
+
+	}
+
+}
