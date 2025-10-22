83625e6df945794a7f1c76695d963455f084bb0e
Raviragul17 <your_github_email@example.com>
2025-10-22 21:04:07 +0530

    i havw complete expect 5 and 6 days

---
+++ CoreJava/src/com/tnsif/dayfour/thirdpackage/PrivateConstructorDemo.java
@@ -0,0 +1,19 @@
+
+package com.tnsif.dayfour.thirdpackage;
+
+public class PrivateConstructorDemo {
+
+	public static void main(String[] s) {
+		// TODO Auto-generated method stub
+		
+		MyClass m=MyClass.getObject();
+		m.setId(10);
+		
+		MyClass m1=MyClass.getObject();
+		
+		System.out.println(m);
+		System.out.println(m1);
+
+	}
+
+}
---
+++ CoreJava/src/com/tnsif/daynine/StringSubClass.java
@@ -0,0 +1,10 @@
+package com.tnsif.daynine;
+
+
+public class StringSubClass extends StringBufferDemo {
+
+	public static void main(String[] args) {
+		
+
+	}
+}
