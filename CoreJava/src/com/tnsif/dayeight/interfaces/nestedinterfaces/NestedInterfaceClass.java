756afde49d6c96f8f78c4e0df950676c866038de
Raviragul17 <your_github_email@example.com>
2025-10-27 10:42:46 +0530

    upto day 10 finish

---
+++ CoreJava/src/com/tnsif/dayeight/interfaces/nestedinterfaces/NestedInterfaceClass.java
@@ -0,0 +1,12 @@
+package com.tnsif.dayeight.interfaces.nestedinterfaces;
+
+public class NestedInterfaceClass implements MyInterface.MyInnerInterface{
+
+	@Override
+	public void print() {
+		 System.out.println("Print method of nested interface");
+	}
+
+	
+
+}
